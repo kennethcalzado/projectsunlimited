003747457c0c6377f275d4fa72fd295b00acb965
leleindistress <139642328+kylaggg@users.noreply.github.com>
2024-03-11 16:29:34 +0800

    arno update

--- public/arno.php
+++ public/arno.php
@@ -6,13 +6,11 @@ ob_start();
 <div id="content">
     <div id="brand-slider cotainer">
         <!-- Slider -->
-        <div data-hs-carousel='{
-    "loadingClasses": "opacity-0",
-    "isAutoPlay": true
-  }' class="relative">
+        <div data-hs-carousel='{"loadingClasses": "opacity-0", "isAutoPlay": true}' class="relative">
             <div class="hs-carousel relative overflow-hidden w-full min-h-[350px] bg-white rounded-lg">
-                <div
-                    class="hs-carousel-body absolute top-0 bottom-0 start-0 flex flex-nowrap transition-transform duration-700 opacity-0">
+                <div>
+                    class="hs-carousel-body absolute top-0 bottom-0 start-0 flex flex-nowrap transition-transform
+                    duration-700 opacity-0">
                     <div class="hs-carousel-slide">
                         <div class="flex justify-center h-full bg-gray-100 p-6">
                             <span class="self-center text-4xl transition duration-700">First slide</span>
