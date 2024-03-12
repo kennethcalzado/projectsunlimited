6045f498440e31861420ee6312d6fcd8ad7b5526
ange <itsangelyne14@gmail.com>
2024-03-12 16:13:19 +0800

    category

--- public/contact.php
+++ public/contact.php
@@ -7,7 +7,7 @@ ob_start();
         <img src="../assets/image/contactus.png" class="w-full h-96 object-cover">
         <div class="absolute inset-0 bg-black opacity-50"></div>
         <div class="absolute inset-0 flex items-center justify-center">
-    <p class="text-[#F6E381] font-bold text-4xl">GET IN TOUCH WITH <br> PROJECTS UNLIMITED</p>
+    <p class="text-[#F6E381] font-bold text-3xl">GET IN TOUCH WITH <br> PROJECTS UNLIMITED</p>
 </div>
     </div>
 </div>
