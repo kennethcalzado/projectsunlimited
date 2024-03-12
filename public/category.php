95ac8741e4a46953c8cf5dfadf1b73a02c602498
ange <itsangelyne14@gmail.com>
2024-03-12 11:47:16 +0800

    category

--- public/category.php
+++ public/category.php
@@ -3,45 +3,62 @@ $pageTitle = "Product Category";
 ob_start();
 ?>
 <div class="content">
-<div class="relative">
-        <img src="../assets/image/stock.png" class="w-full h-32 object-cover">
+    <div class="relative">
+        <img src="../assets/image/stock.png" class="w-full h-96 object-cover">
         <div class="absolute inset-0 bg-black opacity-50"></div>
+        <div class="absolute inset-0 flex items-center justify-center">
+        <p class="text-white font-bold text-4xl">PRODUCTS</p>
     </div>
-<h1 class="text-4xl font-bold text-center text-black bg-white p-4">PRODUCT CATEGORY</h1>
-<div id="categorycontainer">
+    </div>
+    <h1 class="text-4xl font-bold text-center text-black bg-white p-4">PRODUCT CATEGORY</h1>
+    <div id="categorycontainer">
         <div id="prodcat">
             <div class="relative bg-white p-2">
                 <div class="absolute h-30 bg-black "></div>
                 <div class="flex justify-between relative z-10">
-                <div class="flex">
-        <a href="../public/blinds.php" class="w-1/4 bg-white p-4 rounded-lg">
-            <!-- Image 1 -->
-            <img src="../assets/image/blinds.png" alt="Blinds" class="w-full h-32 object-cover">
-        </a>
-        <a href="../public/flooring.php" class="w-1/4 bg-white p-4 rounded-lg">
-            <!-- Image 2 -->
-            <img src="../assets/image/flooring.png" alt="Flooring" class="w-full h-32 object-cover">
-        </a>
-        <a href="../public/wallpaper.php" class="w-1/4 bg-white p-4 rounded-lg">
-            <!-- Image 3 -->
-            <img src="../assets/image/wallpaper.png" alt="Wallpaper" class="w-full h-32 object-cover">
-        </a>
-        <a href="../public/office.php" class="w-1/4 bg-white p-4 rounded-lg">
-            <!-- Image 4 -->
-            <img src="../assets/image/office.png" alt="Office" class="w-full h-32 object-cover">
-        </a>
-    </div>
+                    <div class="flex">
+                        <a href="../public/blinds.php" class="w-1/4 bg-white p-4 pt-3 rounded-lg">
+                            <!-- Image 1 -->
+                            <img src="../assets/image/blinds.png" alt="Blinds" class="w-full h-70 object-cover">
+                        </a>
+                        <a href="../public/flooring.php" class="w-1/4 bg-white p-4 pt-3 rounded-lg">
+                            <!-- Image 2 -->
+                            <img src="../assets/image/flooring.png" alt="Flooring" class="w-full h-70 object-cover">
+                        </a>
+                        <a href="../public/wallpaper.php" class="w-1/4 bg-white p-4 pt-3 rounded-lg">
+                            <!-- Image 3 -->
+                            <img src="../assets/image/wallpaper.png" alt="Wallpaper" class="w-full h-70 object-cover">
+                        </a>
+                        <a href="../public/office.php" class="w-1/4 bg-white p-4 pt-3 rounded-lg">
+                            <!-- Image 4 -->
+                            <img src="../assets/image/office.png" alt="Office" class="w-full h-70 object-cover">
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-
-
-
-<!-- Add more cards as needed -->
+</div>
+<div class="relative">
+    <img src="../assets/image/customizebanner.png" class="w-full h-96 object-cover">
+    <div class="absolute inset-0 bg-black opacity-50"></div>
+    <div class="absolute inset-0 flex items-center justify-center">
+    <p class="text-[#F6E381] font-bold text-4xl">CUSTOMIZE PRODUCTS</p>
+    <p class="text-white font-semibold text-2xl">Many of our products can be customized to the requirements of our clients. These may include the dimensions, colors, textures, and materials used in the item.</p>
+</div>
 
 </div>
-</div> 
+<div id="customcontainer">
+    <h3 class="font-bold text-3xl p-4 px-8">INQUIRE</h3>
+    <p class="font-semibold text-2xl p-4 px-8">Ask about the product and set an official appointment with Projects Unlimited. We are willing to get in touch with you directly and know your ideas.</p>
+    <h3 class="text-right font-bold text-3xl p-4 px-8">PLAN</h3>
+    <p class="text-right font-semibold text-2xl p-4 px-8">Discuss your desired dimension, color, texture, and materials for your customized products and we’ll do it for you. The budget and timeline will be discussed as well.</p>
+    <h3 class="font-bold text-3xl p-4 px-8">CREATE</h3>
+    <p class="font-semibold text-2xl p-4 px-8">Our team will proceed to create your desired products and we’ll give you an estimated time of completion and we’ll keep you updated at all time.</p>
+    <h3 class="text-right font-bold text-3xl p-4 px-8">DELIVER & INSTALL</h3>
+    <p class=" text-right font-semibold text-2xl p-4 px-8">Once the products are completed, we will proceed to delivering and installing the products to your place.</p>
+</div>
+</div>
 <?php
 $content = ob_get_clean();
 include("../public/master.php");
--- public/contact.php
+++ public/contact.php
@@ -4,7 +4,7 @@ ob_start();
 ?>
 <div class="content">
 <div class="relative">
-        <img src="../assets/image/contactus.png" class="w-full h-32 object-cover">
+        <img src="../assets/image/contactus.png" class="w-full h-96 object-cover">
         <div class="absolute inset-0 bg-black opacity-50"></div>
     </div>
 </div>
