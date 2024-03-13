a4e7ab8964e3c7884dbe87f2276639ff88b586cb
kennethcalzado <kennethcalzado16@gmail.com>
2024-03-13 08:59:25 +0800

    hahaha

--- public/master.php
+++ public/master.php
@@ -9,6 +9,7 @@
     </title>
     <!-- Link to your Tailwind CSS file -->
     <link href="../assets/output.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body class="bg-white">
