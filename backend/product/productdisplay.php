72f5cd2afa956306b5cb3d921e6453d2ade42468
itsangelyne <itsangelyne14@gmail.com>
2024-04-04 21:49:14 +0800

    woah woah woah
    filters n search iz working perfectly ehey #tyt #babysteps #smolprogress

--- backend/product/productdisplay.php
+++ backend/product/productdisplay.php
@@ -11,19 +11,35 @@ if ($conn) {
     FROM product p 
     LEFT JOIN brands b ON p.brand_id = b.brand_id 
     LEFT JOIN productcategory pc ON p.CategoryID = pc.CategoryID
-    WHERE p.image_urls IS NOT NULL AND p.image_urls != ' '";
+    WHERE p.image_urls IS NOT NULL AND p.image_urls != ''";
 
     // Check if any filter parameters are provided
     if (isset($_GET['categoryId']) && $_GET['categoryId'] !== '') {
         // Add category filter to the SQL query
         $categoryId = mysqli_real_escape_string($conn, $_GET['categoryId']);
-        $sql .= " AND p.CategoryID = '$categoryId '";
+        if ($categoryId !== 'categoryreset') {
+            $sql .= " AND p.CategoryID = '$categoryId'";
+        }
     }
 
     if (isset($_GET['brandId']) && $_GET['brandId'] !== '') {
         // Add brand filter to the SQL query
         $brandId = mysqli_real_escape_string($conn, $_GET['brandId']);
-        $sql .= " AND p.brand_id = '$brandId'";
+        if ($brandId !== 'brandsreset') {
+            $sql .= " AND p.brand_id = '$brandId'";
+        }
+    }
+
+    // Check if search term is provided
+    if (isset($_GET['searchQuery']) && !empty($_GET['searchQuery'])) {
+        // Sanitize and escape the search term to prevent SQL injection
+        $searchTerm = mysqli_real_escape_string($conn, $_GET['searchQuery']);
+        // Add search filter to the SQL query
+        $sql .= " AND (p.ProductName LIKE '%$searchTerm%' 
+               OR b.brand_name LIKE '%$searchTerm%' 
+               OR p.Description LIKE '%$searchTerm%' 
+               OR pc.CategoryName LIKE '%$searchTerm%'
+               OR p.created_at LIKE '%$searchTerm%')";
     }
 
     // Check if a sorting option is provided
@@ -43,8 +59,6 @@ if ($conn) {
     // Execute the SQL query
     $result = mysqli_query($conn, $sql);
 
-    // echo $sql;
-
     // Check if the query was executed successfully
     if ($result) {
         // Initialize an array to store the products
@@ -56,7 +70,7 @@ if ($conn) {
             while ($row = mysqli_fetch_assoc($result)) {
                 // Convert image URLs string to an array
                 $imageUrls = explode(',', $row['image_urls']);
-                
+
                 // Remove empty elements from the image URLs array
                 $imageUrls = array_filter($imageUrls);
 
