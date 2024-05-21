16860fc0bbb9e612310f7729139ec61fafd1bf51
kennethcalzado <kennethcalzado16@gmail.com>
2024-05-21 23:10:33 +0800

    testing

--- public/index.php
+++ public/index.php
@@ -13,7 +13,7 @@ include("../backend/conn.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $pageTitle; ?></title>
-    <link rel="stylesheet" href="../assets/input.css">
+    <link rel="stylesheet" href="projectsunlimited/assets/input.css">
 
     <style>
         body,
