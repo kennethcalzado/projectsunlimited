f8501bc194a49720f2aa405f54ccb62cf1c756e5
ange <itsangelyne14@gmail.com>
2024-05-26 13:03:45 +0800

    weblogo

--- public/index.php
+++ public/index.php
@@ -13,6 +13,7 @@ include("../backend/conn.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $pageTitle; ?></title>
+    <link rel="icon" type="image/x-icon" href="../assets/image/PUlogo.png">
     <link rel="stylesheet" href="assets/input.css">
 
     <style>
